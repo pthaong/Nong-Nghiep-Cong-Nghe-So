d531075b7232f1021a80b0e686db4cd502471124
pthaong <khanhng528@gmail.com>
2026-09-20 14:50:14 +0700

    feat(be): initialize backend core

---
+++ backend/src/main/java/com/smartagriculture/backend/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.smartagriculture.backend.repository;
+
+import com.smartagriculture.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
